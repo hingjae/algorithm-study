0bef11e64f7e31bc0d86cf2c8f75e5c040d2e3fd
hingjae <ljh23270@naver.com>
2023-02-14 11:43:14 +0900

    1213

--- c++reload/9375.cpp
+++ c++reload/9375.cpp
@@ -1,24 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int t, n;
-int main() {
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-    cin >> t;
-    while(t--) {
-        cin >> n;
-        map<string, int> mp;
-        for(int i = 0; i < n; i++) {
-            string name, part;
-            cin >> name >> part;
-            mp[part]++;
-        }
-        long long ret = 1;
-        for(auto a : mp) {
-            ret *= ((long long)a.second + 1);
-        }
-        ret--;
-        cout << ret << "\n";
-    }
-    return 0;
+string a, b;
+int main(){
+	cin >> t;
+	while(t--){
+		cin >> n;
+		map<string, int> _map;
+		for(int i = 0; i < n; i++){
+			cin >> a >> b;
+			_map[b]++;
+		}
+		long long ret = 1;
+		for(auto c : _map)
+			ret *= ((long long)c.second + 1);
+		ret--;
+		cout << ret << '\n';
+	}
 }
