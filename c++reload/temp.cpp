69b0abfc6a4a643853cf10b877c2070070336459
honey170 <63653475+hingjae@users.noreply.github.com>
2023-02-15 11:26:04 +0900

    1940, 3986

--- c++reload/temp.cpp
+++ c++reload/temp.cpp
@@ -1,32 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int arr[9], sum;
-vector<int> v;
-pair<int, int> ret;
-void solve() {
-	for(int i = 0 ; i < 9; i++) {
-		for (int j = 0; j < i; j++) {
-			if(sum - arr[i] - arr[j] == 100) {
-				ret = {i, j};
-				return;
-			}
-		}
-	}
-}
+int t, n;
+string name, part;
 int main() {
-	for(int i = 0; i < 9; i++) {
-		cin >> arr[i]; sum += arr[i];
-	}
-	solve();
-	for(int i = 0; i < 9; i++){
-		if(ret.first == i || ret.second == i) {
-			continue;
+	cin >> t;
+	while(t--) {
+		map<string, int> mp;
+		cin >> n;
+		for(int i = 0; i < n; i++ ) {
+			cin >> name >> part;
+			mp[part]++;
 		}
-		v.push_back(arr[i]);
+		int ret = 1;
+		for(auto a : mp) {
+			ret *= (a.second + 1);
+		}
+		cout << ret - 1 << "\n";
 	}
-	sort(v.begin(), v.end());
-	for(int a : v) {
-		cout << a << "\n";
-	}
-	return 0;
 }
