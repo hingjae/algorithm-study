279cec4e2e6e167a21ee131402153e94fc9185a9
hingjae <ljh23270@naver.com>
2023-04-16 00:15:31 +0900

    2109 1285

--- baekjoon/temp.cpp
+++ baekjoon/temp.cpp
@@ -1,29 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-vector<vector<int>> combis;
-vector<int> v;
-void combi(int n, int k, int idx) {
-
-	if(v.size() == k) {
-		combis.push_back(v);
-		return;
-	}
-
-	for(int i = idx; i <= n; i++) {
-		v.push_back(i);
-		combi(n, k, i + 1);
-		v.pop_back();
-	}
-}
+map<int, int> mp;
 int main() {
-
-	combi(3, 2, 1);
-	for(auto v : combis) { 
-		for(auto item : v) {
-			cout << item  << " ";
-		}
-		cout << "\n";
+	cout << mp[0] << "\n";
+	cout << mp[1] << "\n";
+	for(auto item : mp) {
+		cout << item.first << " " << item.second << "\n";
 	}
 	return 0;
 }
---
+++ baekjoon/unsolved/1285.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+void go(int here) {
+    if(here == n + 1) {
+        int sum = 0;
+        for(int i = 1; i <= (1 << (n - 1)); i *= 2)
+    }
+}
